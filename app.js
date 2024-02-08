0738996f24c4176683f135751aacb7e6e1d3eda3
RalfiSlask <nilssonnarki@gmail.com>
2024-02-08 11:16:39 +0100

    feat: add mongodb connection / outcommented for now

--- app.js
+++ app.js
@@ -2,13 +2,24 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const MongoClient = require('mongodb').MongoClient;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-var tweetsRouter = require('./routes/tweets');
-var tagsRouter = require('./routes/tweets');
 
 var app = express();
+
+/* MongoClient.connect('')
+  .then((client) => {
+    console.log('We are connected to database');
+
+    const db = client.db('matte-diem');
+    app.locals.db = db;
+  })
+  .catch((err) => {
+    console.log(err, 'could not connect to database');
+    process.exit(1);
+  }); */
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -18,7 +29,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/tweets', tweetsRouter);
-app.use('/tags', tagsRouter);
 
 module.exports = app;
