6697d14cb70f165abc89a4c9fc696308bb985b69
Diem Bang <diembang09@gmail.com>
2024-02-08 11:53:27 +0100

    create get endpoints

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
+let users = [
+  {"user": "Janne"}
+]
+
+/* GET all users */
+router.get('/', function(req, res) {
+  res.send('users');
+
+});
+
+/* GET specific user */
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
